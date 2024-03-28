ce89191d5ebf7f01c54601de784f82e085713877
Hugo Culot <hugoculot.dev@gmail.com>
2024-03-28 19:26:18 +0100

    Last Version of QuiEstLePremier.

--- sketch.js
+++ sketch.js
@@ -1,35 +1,23 @@
 const ledSound = new Audio('audios/ledOn.wav');
+const errorSound = new Audio('audios/error.wav');
 
 let nbrErrors = 0;
 let nbrWins = 0;
+let gameEnded = false; // Variable pour indiquer si le jeu est terminé ou non
 
 document.addEventListener("DOMContentLoaded", function () {
   resetGame();
 });
 
-/* Pour johnny
-function resolve() {
-  console.log("resolve");
-      $.ajax({
-          url: `functions/functionsDatabase.php?action=resolve&idPartie=${idPartie}&idModule=${idModule}`,
-          success: function(data) {
-              $('#result').html(data);
-          }
-      });
-  }
-function incrementError() {
-  $.ajax({
-          url: `functions/functionsDatabase.php?action=incrementError&idPartie=${idPartie}&idModule=${idModule}`,
-          success: function(data) {
-              $('#result').html(data);
-          }
-      });
-  }
-  */
-  function ledOnClickSound () {
-    ledSound.currentTime = 0;
-    ledSound.play(); 
-  };
+function ledOnClickSound () {
+  ledSound.currentTime = 0;
+  ledSound.play(); 
+};
+
+function errorOnClickSound () {
+  errorSound.currentTime = 0;
+  errorSound.play(); 
+};
 
 function resetGame() {
   $.ajax({
@@ -40,37 +28,36 @@ function resetGame() {
       const resultButton = document.querySelector(".result");
 
       resultButton.addEventListener("click", function () {
-        nbrWins++
-        if (nbrWins === 1) {
-          console.log("Encore 2 fois pour gagner");
-          document.querySelector('.led-etape-1').src = 'assets/ledVerteEtape.png';
-          ledOnClickSound();
-        };
-
-        if (nbrWins === 2) {
-          console.log("Encore 1 fois pour gagner");
-          document.querySelector('.led-etape-2').src = 'assets/ledVerteEtape.png';
-          ledOnClickSound();
-        };
-
-        if (nbrWins === 3) {
-          console.warn("Gagner fin du module!, appeller en ajax la fonction Resolve de johnny");
-          //resolve(); Pour Johnny
-          document.querySelector('.led-etape-3').src = 'assets/ledVerteEtape.png';
-          document.querySelector('.led img').src = 'assets/ledVerte.png';
-          ledOnClickSound();
-        };
-        resetGame();
+        if (!gameEnded) { // Vérifier si le jeu est terminé
+          nbrWins++;
+          if (nbrWins === 1) {
+            console.log("Encore 2 fois pour gagner");
+            document.querySelector('.led-etape-1').src = 'assets/ledVerteEtape.png';
+            ledOnClickSound();
+          } else if (nbrWins === 2) {
+            console.log("Encore 1 fois pour gagner");
+            document.querySelector('.led-etape-2').src = 'assets/ledVerteEtape.png';
+            ledOnClickSound();
+          } else if (nbrWins === 3) {
+            console.warn("Gagner fin du module!, appeller en ajax la fonction Resolve de johnny");
+            document.querySelector('.led-etape-3').src = 'assets/ledVerteEtape.png';
+            document.querySelector('.led img').src = 'assets/ledVerte.png';
+            ledOnClickSound();
+            gameEnded = true; // Marquer le jeu comme terminé
+          }
+          resetGame();
+        }
       });
 
       const otherWords = document.querySelectorAll("td:not(.result)");
 
       otherWords.forEach(function (word) {
         word.addEventListener("click", function () {
-          if (nbrErrors < 1) {
+          if (!gameEnded && nbrErrors < 1) { // Vérifier si le jeu est terminé et le nombre d'erreurs
             console.log("Une chance restante...");
             console.warn("Erreur, appeller en ajax la function IncrementError de johnny");
             nbrErrors++;
+            errorOnClickSound();
             //incrementError(); (Pour Johnny)
           } else {
             console.warn("Perdu, fin du Jeu!");
